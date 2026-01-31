b69888d021aa7f01ab920b88ccafe249258df5a0
Andreas <niessl.andreas@gmail.com>
2026-01-31 04:16:06 +0100

    Komplett neugemacht Drecksscheissefotze8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router base="./">
+        <Router base="/">
           <AppRouter />
         </Router>
       </TooltipProvider>
