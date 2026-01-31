3fbaa3e4331f0a511a791e24ce276221631089a0
Andreas <niessl.andreas@gmail.com>
2026-01-31 02:54:38 +0100

    Komplett neugemacht Drecksscheissefotze

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-   base:'/Flyer-Design/', // Dein Repository-Name
+   base:'./', // Dein Repository-Name
   plugins: [
     react(),
     runtimeErrorOverlay(),
